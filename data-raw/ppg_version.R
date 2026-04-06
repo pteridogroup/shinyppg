215febd4a1ac3acb942f3b43730b77fc10307b81
joelnitta <joelnitta@gmail.com>
2026-04-06 13:56:30 +0900

    Update to PPG v0.0.0.9005

--- data-raw/ppg_version.R
+++ data-raw/ppg_version.R
@@ -1,6 +1,6 @@
 ## code to prepare `ppg_version` dataset goes here
 
 # PPG version used in ppg_full
-ppg_version <- "0.0.0.9002"
+ppg_version <- "0.0.0.9005"
 
 usethis::use_data(ppg_version, overwrite = TRUE)
